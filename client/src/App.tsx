9ceb8b9c1042ffddfd6e350340bd4e34cdef015d
Martin Krečmer <martin.krecmer@gmail.com>
2025-04-15 20:55:42 +0200

    Obnovený pôvodný App.tsx s routovaním

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,8 @@
+import { RouterProvider } from 'react-router-dom'
+import { router } from './routes'
+
 function App() {
-  return <div>Aplikácia funguje! 🟢</div>
+  return <RouterProvider router={router} />
 }
 
 export default App
